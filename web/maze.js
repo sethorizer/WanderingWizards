9c4f2f70ef734a6d60bf116c8882d5ff2a30494c
instafluff <instaflufftv@gmail.com>
2019-08-28 21:06:54 -0700

    Super Fast Pathfinding

--- web/maze.js
+++ web/maze.js
@@ -1,11 +1,6 @@
-function findTheWay( grid, gridWidth, gridHeight, x, y, gX, gY, paths = [], travelDist = 0, discoveredMinDist = 99 ) {
-  // console.log( x, y, travelDist );
-  if( travelDist > discoveredMinDist ) {
-    return null;
-  }
-  // console.log( x, y, grid );
+function findTheWay( grid, gridWidth, gridHeight, x, y, gX, gY ) {
   if( x === gX && y === gY ) {
-    return paths.concat( { x, y } );
+    return [ { x, y } ];
   }
   if( x < 0 || x >= gridWidth ||
       y < 0 || y >= gridHeight ) {
@@ -14,87 +9,38 @@ function findTheWay( grid, gridWidth, gridHeight, x, y, gX, gY, paths = [], trav
   if( grid[ y * gridWidth + x ] > 0 ) {
     return null;
   }
+
+  var next = [ { x, y, score: calcScore( x, y, gX, gY ), path: [] } ];
   var gridCopy = grid.slice();
-  gridCopy[ y * gridWidth + x ] = 2;
-  // // North, East, South, West
-  // var north = findTheWay( gridCopy, gridWidth, gridHeight, x, y - 1, gX, gY, paths.concat( { x, y } ) );
-  // var east = findTheWay( gridCopy, gridWidth, gridHeight, x + 1, y, gX, gY, paths.concat( { x, y } ) );
-  // var south = findTheWay( gridCopy, gridWidth, gridHeight, x, y + 1, gX, gY, paths.concat( { x, y } ) );
-  // var west = findTheWay( gridCopy, gridWidth, gridHeight, x - 1, y, gX, gY, paths.concat( { x, y } ) );
-  //
-  // // Figure out minimum valid path length
-  // var minPath = north;
-  // if( east ) {
-  //   if( !minPath ) { minPath = east; }
-  //   else if( east.length < minPath.length ) {
-  //     minPath = east;
-  //   }
-  // }
-  // if( south ) {
-  //   if( !minPath ) { minPath = south; }
-  //   else if( south.length < minPath.length ) {
-  //     minPath = south;
-  //   }
-  // }
-  // if( west ) {
-  //   if( !minPath ) { minPath = west; }
-  //   else if( west.length < minPath.length ) {
-  //     minPath = west;
-  //   }
-  // }
-  // return minPath;
-
-  // // OPTIMIZED VERSION!
-  // // North, East, South, West
-  // var north = findTheWay( gridCopy, gridWidth, gridHeight, x, y - 1, gX, gY, paths.concat( { x, y } ), travelDist + 1, discoveredMinDist );
-  // if( north && north.length < discoveredMinDist ) { discoveredMinDist = north.length; }
-  // var east = findTheWay( gridCopy, gridWidth, gridHeight, x + 1, y, gX, gY, paths.concat( { x, y } ), travelDist + 1, discoveredMinDist );
-  // if( east && east.length < discoveredMinDist ) { discoveredMinDist = east.length; }
-  // var south = findTheWay( gridCopy, gridWidth, gridHeight, x, y + 1, gX, gY, paths.concat( { x, y } ), travelDist + 1, discoveredMinDist );
-  // if( south && south.length < discoveredMinDist ) { discoveredMinDist = south.length; }
-  // var west = findTheWay( gridCopy, gridWidth, gridHeight, x - 1, y, gX, gY, paths.concat( { x, y } ), travelDist + 1, discoveredMinDist );
-
-  // // Figure out minimum valid path length
-  // var minPath = north;
-  // if( east ) {
-  //   if( !minPath ) { minPath = east; }
-  //   else if( east.length < minPath.length ) {
-  //     minPath = east;
-  //   }
-  // }
-  // if( south ) {
-  //   if( !minPath ) { minPath = south; }
-  //   else if( south.length < minPath.length ) {
-  //     minPath = south;
-  //   }
-  // }
-  // if( west ) {
-  //   if( !minPath ) { minPath = west; }
-  //   else if( west.length < minPath.length ) {
-  //     minPath = west;
-  //   }
-  // }
-  // return minPath;
-
-  // OPTIMIZED VERSION 2!
-  var nextPaths = [
-    { x: x, y: y - 1, score: Math.abs( gX - x ) + Math.abs( gY - ( y - 1 ) ) },
-    { x: x + 1, y: y, score: Math.abs( gX - ( x + 1 ) ) + Math.abs( gY - y ) },
-    { x: x, y: y + 1, score: Math.abs( gX - x ) + Math.abs( gY - ( y + 1 ) ) },
-    { x: x - 1, y: y, score: Math.abs( gX - ( x - 1 ) ) + Math.abs( gY - y ) },
-  ];
-  nextPaths.sort( ( a, b ) => a.score - b.score );
-  // console.log( nextPaths );
-  var minPath = null;
-  nextPaths.forEach( p => {
-    // console.log( p );
-    var path = findTheWay( gridCopy, gridWidth, gridHeight, p.x, p.y, gX, gY, paths.concat( { x, y } ), travelDist + 1, discoveredMinDist );
-    if( path && path.length < discoveredMinDist ) {
-      discoveredMinDist = path.length;
-      return minPath;
+  while( next.length > 0 ) {
+    // Check the top item and evaluate
+    var p = next.shift();
+    if( p.x === gX && p.y === gY ) {
+      return p.path.concat( { x: p.x, y: p.y } );
     }
-  });
-  return minPath;
+    if( p.x < 0 || p.x >= gridWidth ||
+        p.y < 0 || p.y >= gridHeight ) {
+      // Skip!
+    }
+    else if( gridCopy[ p.y * gridWidth + p.x ] > 0 ) {
+      // Wall!
+    }
+    else {
+      gridCopy[ p.y * gridWidth + p.x ] = 2;
+      var path = p.path.concat( { x: p.x, y: p.y } );
+      next.push( { x: p.x, y: p.y - 1, score: calcScore( p.x, p.y - 1, gX, gY ), path: path } );
+      next.push( { x: p.x + 1, y: p.y, score: calcScore( p.x + 1, p.y, gX, gY ), path: path } );
+      next.push( { x: p.x, y: p.y + 1, score: calcScore( p.x, p.y + 1, gX, gY ), path: path } );
+      next.push( { x: p.x - 1, y: p.y, score: calcScore( p.x - 1, p.y, gX, gY ), path: path } );
+      // Sort next paths
+      next.sort( (a, b) => calcScore( a.x, a.y, gX, gY ) - calcScore( b.x, b.y, gX, gY ) );
+    }
+  }
+  return null;
+}
+
+function calcScore( x, y, gX, gY ) {
+  return Math.abs( gX - x ) + Math.abs( gY - y );
 }
 
 var verticalWalls = [
